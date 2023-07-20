64093a26fe20283e19476b97a67a16408b56a284
levbitz <maxinosamx@gmail.com>
2023-07-20 22:16:56 +0530

    read me

---
+++ README.md
@@ -0,0 +1,8 @@
+# whatsApp CLONE
+
+### React Native
+### Expo 
+### Firebase 
+
+ ### Note
+ Firebase firestore now introudce the OR  close you can use that
--- src/Lib/Firebase/Firebase.js
+++ src/Lib/Firebase/Firebase.js
@@ -8,13 +8,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage"
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-YCfi7ZvODTg8dWTnIL744us8tUl9xSg",
-  authDomain: "demoproject-903e6.firebaseapp.com",
-  databaseURL: "https://demoproject-903e6-default-rtdb.firebaseio.com",
-  projectId: "demoproject-903e6",
-  storageBucket: "demoproject-903e6.appspot.com",
-  messagingSenderId: "372047678779",
-  appId: "1:372047678779:web:8eea30beddec67f3c13b1e"
+
 };
 
 // Initialize Firebase
